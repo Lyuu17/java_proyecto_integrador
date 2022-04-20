1b4593d149c328987358777c35e0234c0e129431
Lyuu <14199521+Lyuu17@users.noreply.github.com>
2022-04-20 12:37:40 +0200

    app: implementar modelo de Registro

--- src/controller/RegistroController.java
+++ src/controller/RegistroController.java
@@ -2,21 +2,19 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 import javax.swing.JOptionPane;
 import app.Cuenta;
-import app.GestorBD;
 import app.Principal;
+import model.RegistroModel;
 import view.RegistroView;
 
 public class RegistroController {
+	private RegistroModel model;
 	private RegistroView view;
 	
 	public RegistroController() {
-        this.view = new view.RegistroView();
+		this.model = new RegistroModel();
+        this.view = new RegistroView();
         this.view.setTitle(Principal.PROGRAMA_NOMBRE);
         this.view.addEnviarListener(new Enviar());
     }
@@ -37,29 +35,15 @@ public class RegistroController {
 				JOptionPane.showMessageDialog(view.getContentPane(), "Faltan campos por rellenar", "Registro", JOptionPane.ERROR_MESSAGE);
 				return;
 			}
-
-			Connection conn = GestorBD.getConexion();
-			PreparedStatement stmt;
-			try {
-				String hashContraseña = utils.HashContraseña.hash(view.getContraseña());
-
-				stmt = conn.prepareStatement("INSERT INTO usuarios (id, usuario, contraseña) VALUES (ID_AUTO_INCREMENT_SEQ.NEXTVAL, ?, ?)");
-
-				GestorBD.consulta(stmt, view.getUsuario(), hashContraseña);
-
-				stmt = conn.prepareStatement("INSERT INTO datos_usuarios (id, nombre, apellidos, email, tlf, direccion, ciudad, codigopostal) VALUES (ID_AUTO_INCREMENT_SEQ.CURRVAL, ?, ?, ?, ?, ?, ?, ?)");
-
-				GestorBD.consulta(stmt, view.getNombre(), view.getApellidos(), view.getEmail(), view.getNumTlf(), view.getDireccion(), view.getCiudad(), view.getCodigoPostal());
-
-				Cuenta.setIniciadoSesion(true);
-
-				JOptionPane.showMessageDialog(view.getContentPane(), "OK", "Registro", JOptionPane.PLAIN_MESSAGE);
-
-				//dispose();
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+			
+			if (!model.registrarUsuario(view.getUsuario(), view.getContraseña(), 
+					view.getNombre(), view.getApellidos(), view.getEmail(), view.getNumTlf(), view.getDireccion(), view.getCiudad(), view.getCodigoPostal())) {
+				return;
 			}
+			
+			Cuenta.setIniciadoSesion(true);
+
+			JOptionPane.showMessageDialog(view.getContentPane(), "OK", "Registro", JOptionPane.PLAIN_MESSAGE);
 		}
 	}
 }
---
+++ src/model/RegistroModel.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import app.Cuenta;
+import app.GestorBD;
+
+public class RegistroModel {
+	public RegistroModel() {
+	}
+	
+	public boolean registrarUsuario(String usuario, String contraseña, String nombre, String apellidos, String email, String numTlf, String direccion, String ciudad, String codigoPostal) {
+		Connection conn = GestorBD.getConexion();
+		PreparedStatement stmt;
+		try {
+			String hashContraseña = utils.HashContraseña.hash(contraseña);
+
+			stmt = conn.prepareStatement("INSERT INTO usuarios (id, usuario, contraseña) VALUES (ID_AUTO_INCREMENT_SEQ.NEXTVAL, ?, ?)");
+
+			GestorBD.consulta(stmt, usuario, hashContraseña);
+
+			stmt = conn.prepareStatement("INSERT INTO datos_usuarios (id, nombre, apellidos, email, tlf, direccion, ciudad, codigopostal) VALUES (ID_AUTO_INCREMENT_SEQ.CURRVAL, ?, ?, ?, ?, ?, ?, ?)");
+
+			GestorBD.consulta(stmt, nombre, apellidos, email, numTlf, direccion, ciudad, codigoPostal);
+
+			Cuenta.setIniciadoSesion(true);
+			
+			return true;
+
+			//dispose();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		return false;
+	}
+}
